c14fe91a409d3edb061c4b85eab8effafe1bda6b
dylan-morrissey <dylanmorrissey2010@gmail.com>
2019-11-19 11:54:39 +0000

    Added GUI for Server.

--- src/Server.java
+++ src/Server.java
@@ -19,6 +19,28 @@ public class Server extends JFrame {
   }
 
   public Server() {
+    // Place text area on the frame
+    setLayout(new BorderLayout());
+    add(new JScrollPane(jta), BorderLayout.CENTER);
 
+    setTitle("Server");
+    setSize(500, 300);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true); // It is necessary to show the frame here!
+
+    try {
+      // Create a server socket
+      ServerSocket serverSocket = new ServerSocket(8000);
+      jta.append("Server started at " + new Date() + '\n');
+
+      // Listen for a connection request
+      
+      while (true) {
+    	  Socket socket = serverSocket.accept();
+    	  jta.append("Server: Processing......\n");
+        }
+    } catch(IOException ex) {
+      System.err.println(ex);
+    }
   }
 }
